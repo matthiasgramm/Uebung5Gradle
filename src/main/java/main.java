f341e2bd154b0642110a547b02e1b879cd10ebec
Matthias Gramm <matthiasgramm@Matthiass-MBP.fritz.box>
2017-01-21 21:48:59 +0100

    Repo restructirng

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,17 @@
+package minigame;
 
-public class main
+import fhfl.miniGame.engine.MiniGame;
+import fhfl.miniGame.io.IOFactory;
+
+/**
+ * This class is used to start a {@link MiniGame}. There is no need to edit this file. If you want to debug you project you can use this file as starting point.
+ * 
+ */
+public class Main
 {
-
-	public static void main(String[] args) {
-		
-		 MyFrame obj_myFrame = new MyFrame();
-		
+	public static void main(String[] args)
+	{
+		// Create a new player with the given game.
+		IOFactory.getDefaultIOFactory().getMiniGamePlayer(new MyMiniGame());
 	}
 }
